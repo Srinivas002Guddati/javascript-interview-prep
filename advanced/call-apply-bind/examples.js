9f056c925d082b67a48225e4d3bdb02b9593c653
Srinivas G <srinivasg.mca002@gmail.com>
2025-09-15 17:57:41 +0530

    call and appy examples added

--- advanced/call-apply-bind/examples.js
+++ advanced/call-apply-bind/examples.js
@@ -13,3 +13,25 @@ function introduce(role, location){
 //using call
 introduce.call(dev, 'Technical Lead', 'Bangalore');
 introduce.apply(designer, ['UX Designer', "Bangalore"]);
+
+//Example: Borrowing Methods from Native Prototypes
+
+// const args = [ 1,2,3];
+
+// function sum(){
+//     return Array.prototype.reduce.apply(arguments, (acc, val)=> acc+val, 0)
+// }
+
+// console.log(sum.apply(null, args));
+
+//Example: Logging with Context Injection
+
+function logAction(action){
+    console.log(`[${this.module} ${action}]`);
+}
+
+const authModule = { module: "Auth"};
+const uiModule = { module: 'UI'}
+
+logAction.call(authModule, "User login");
+logAction.call(uiModule, "Button cliked");
