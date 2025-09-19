85f8440d8e2d1ad9a9b93a7cf4d3c6d22a537f27
Srinivas G <srinivasg.mca002@gmail.com>
2025-09-19 14:38:16 +0530

    recursion example added

---
+++ coding-challenges/recursion/recursion-basics.js
@@ -0,0 +1,41 @@
+
+/* What is Recursion:
+    Function calls itself to solve smaller versions of the same problem.
+
+    Two parts of Recursion:
+
+    1 - Base case: stop calling itself(when to stop calling itself)
+    2 - Recursive case: part where function calls itself
+*/
+
+//Print n to 1 using recursion
+
+function print(n){
+
+    if(n == 0) return;
+    console.log(n);
+    print(n-1);
+}
+
+print(5);
+
+
+//Print 1 to n using recursion
+let n = 5;
+function print1(x){
+
+    if(x > n) return;
+    console.log(x);
+    x = x+1;
+    print1(x);
+}
+
+print1(1);
+
+/*
+Common Mistakes of Recursion:
+ 1: Missing Base case - Stack overflow
+ 2: Not simplyfying the input - never reaches base case
+ 3: Too deep recursion - large inputs
+ 4: Keeping in mind the time complexity
+*/
